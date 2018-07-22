347363603f40ef0ce5c363b4547b9e792ee7fbdb
nyakasv <nyakasv@gmail.com>
2018-07-22 21:06:02 +0200

    Initial commit

---
+++ DatingApp.SPA/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { User } from './_models/User';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'dating app';
+  jwtHelper: JwtHelperService = new JwtHelperService();
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    if (token) {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+    if (user) {
+      this.authService.currentUser = user;
+      if (this.authService.currentUser.photoUrl !== null) {
+        this.authService.changeMemberPhoto(user.photoUrl);
+      } else {
+        this.authService.changeMemberPhoto('../assets/user.png');
+      }
+    }
+  }
+}
+
+
+
